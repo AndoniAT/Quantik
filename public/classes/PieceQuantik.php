c1dc34a5df2f24ba0e31cdf8e0680b1a326c1301
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-01-14 10:58:17 +0100

    Creation de methodes et constructeur pour la classe PieceQuantik

--- public/classes/PieceQuantik.php
+++ public/classes/PieceQuantik.php
@@ -1,5 +1,67 @@
 <?php
  class PieceQuantik {
+    static public int $white = 0;
+    static public int $black = 0;
+    static public int $void = 0;
+    static public int $cube = 1;
+    static public int $cone = 2;
+    static public int $cylindre = 3;
+    static public int $sphere = 4;
+    protected int $forme;
+    protected int $couleur;
+
+    private function __contructor(int $forme, int $couleur) {
+        $this->forme = $forme;
+        $this->couleur = $couleur;
+    }
+
+    public function getForme() : int {
+        return $this->forme;
+    }
+
+    public function getCouleur() : int {
+        return $this->couuleur;
+    }
+
+    public function __toString() : string {
+    }
+
+    public static function initVoid() : PieceQuantik {
+
+    }
+
+    public static function initWhiteCube() : PieceQuantik {
+
+    }
+
+    public static function initBlackCube() : PieceQuantik {
+
+    }
+
+    public static function initWhiteCone() : PieceQuantik {
+
+    }
+
+
+    public static function initBlackCone() : PieceQuantik {
+
+    }
+
+    public static function initWhiteCylindre() : PieceQuantik {
+
+    }
+
     
- }
+    public static function initBlackCylindre() : PieceQuantik {
+
+    }
+    
+    public static function initWhiteSphere() : PieceQuantik {
+
+    }
+
+    public static function initBlackSphere() : PieceQuantik {
+
+    }
+}
 ?>
