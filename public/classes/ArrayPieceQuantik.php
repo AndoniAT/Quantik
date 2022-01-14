c4eba0996f5658eb60b6572ac3a1b88e1faa7c7c
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-01-14 11:31:58 +0100

    Creation fonctions et attributs pour la classe ArrayPieceQuantik

--- public/classes/ArrayPieceQuantik.php
+++ public/classes/ArrayPieceQuantik.php
@@ -1,5 +1,48 @@
 <?php
+    require_once('PieceQuantik.php');
+
     class ArrayPieceQuantik{
-        
+        protected array $piecesQuantiks;
+        protected int $taille;
+
+        public function __construct() {
+
+        }
+
+        public function __toString() : string {
+
+        }
+
+        public function getPieceQuantik(int $pos) : PieceQuantik {
+
+        }
+
+        public function setPieceQuantik(int $pos, PieceQuantik $piece) : void {
+
+        }
+
+        public function addPieceQuantik(PieceQuantik $piece) : void {
+
+        }
+
+        public function removePieceQuantik(int $pos) : void {
+
+        }
+
+        public function getTaille() : int {
+            return $this->taille;
+        }
+
+        public function setTaille(int $taille) : void {
+            $this->taille = $taille;
+        }
+
+        public static function initPiecesNoires() : ArrayPieceQuantik {
+
+        }
+
+        public static function initPiecesBlanches() : ArrayPieceQuantik {
+
+        }
     }
 ?>
