efbf729a89f5c6bd7dd7154760b56885589c2c7a
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-01-14 22:49:50 +0100

    Mis a jour des commentaires

--- public/classes/PieceQuantik.php
+++ public/classes/PieceQuantik.php
@@ -1,28 +1,52 @@
 <?php
+    /**
+     * Class PieceQuantik qui nous permet de creer une nouvelle pièce pour le jeu.
+     */
  class PieceQuantik {     
+    
+    // Couleurs ============
     public const  WHITE = 0;
     public const  BLACK = 1;
-    public const  VOID = 0;
+
+    // Formes ==============
     public const  CUBE = 1;
     public const  CONE = 2;
     public const  CYLINDRE = 3;
     public const  SPHERE = 4;
+
+    // Absence ============
+    public const  VOID = 0;
+
+    // Attributs de chaque pièce ==========
     protected int $forme;
     protected int $couleur;
 
+    /**
+     * Le constructeur est par défaut privé, donc pour créer une pièce
+     * il faut passe forcement par nos méthodes statiques.
+     */
     private function __construct(int $forme, int $couleur) {
         $this->forme = $forme;
         $this->couleur = $couleur;
     }
 
+    /**
+     * Obtenir la forme de la piece
+     */
     public function getForme() : int {
         return $this->forme;
     }
 
+    /**
+     * Obtenir la couleur de la piece
+     */
     public function getCouleur() : int {
         return $this->couleur;
     }
 
+    /**
+     * Affichage de la pièce
+     */
     public function __toString() : string {
         $str = "Forme : ";
 
@@ -68,38 +92,65 @@
         return $str;
     }
 
+    /**
+     * Pour initialiser une pièce vide
+     */
     public static function initVoid() : static {
         return new self(self::VOID, self::VOID);    
     }
 
+    /**
+     * Pour initialiser une pièce de forme Cube blanche
+     */
     public static function initWhiteCube() {
         return new self(self::CUBE, self::WHITE);
     }
 
+    /**
+     * Pour initialiser une pièce de forme Cube noire
+     */
     public static function initBlackCube() : PieceQuantik {
         return new self(self::CUBE, self::BLACK);
     }
 
+    /**
+     * Pour initialiser une pièce de forme Cone blanche
+     */
     public static function initWhiteCone() : PieceQuantik {
         return new self(self::CONE, self::WHITE);
     }
 
+    /**
+     * Pour initialiser une pièce de forme Cone noire
+     */
     public static function initBlackCone() : PieceQuantik {
         return new self(self::CONE, self::BLACK);
     }
-
+    
+    /**
+     * Pour initialiser une pièce de forme Cylinde blanche
+     */
     public static function initWhiteCylindre() : PieceQuantik {
         return new self(self::CYLINDRE, self::WHITE);
     }
 
+    /**
+     * Pour initialiser une pièce de forme Cylindre Noire
+     */
     public static function initBlackCylindre() : PieceQuantik {
         return new self(self::CYLINDRE, self::BLACK);
     }
     
+    /**
+     * Pour initialiser une pièce de forme Sphere blanche
+     */
     public static function initWhiteSphere() : PieceQuantik {
         return new self(self::SPHERE, self::WHITE);
     }
 
+    /**
+     * Pour initialiser une pièce de forme Sphere Noire
+     */
     public static function initBlackSphere() : PieceQuantik {
         return new self(self::SPHERE, self::BLACK);
     }
