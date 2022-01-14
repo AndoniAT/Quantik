1c4d5a81a395d3e1358b25939635bc492a3d3656
bm190247 <mathieu.barton@etu.univ-lehavre.fr>
2022-01-14 10:56:18 +0100

    Creation des méthodes de ActionQuantik

--- public/classes/ActionQuantik.php
+++ public/classes/ActionQuantik.php
@@ -2,5 +2,76 @@
     class ActionQuantik{
         protected PlateauQuantik $plateau;
 
+        /**
+         * @param PlateauQuantik $plateau
+         */
+        public function __construct(PlateauQuantik $plateau)
+        {
+            $this->plateau = $plateau;
+        }
+
+        /**
+         * @return PlateauQuantik
+         */
+        public function getPlateau(): PlateauQuantik
+        {
+            return $this->plateau;
+        }
+
+        public function isRowWin(int $numRow) : bool {
+
+            //TODO
+
+            return true;
+        }
+
+        public function isColWin(int $numCol) : bool {
+
+            //TODO
+
+            return true;
+        }
+
+        public function isCornerWin(int $numCorner) : bool {
+
+            //TODO
+
+            return true;
+        }
+
+        public function isValidePose(int $numRow , int $colNum , PieceQuantik $piece ) : bool {
+
+            //TODO
+
+            return true;
+        }
+
+        public function posePiece(int $numRow , int $colNum , PieceQuantik $piece ) : void {
+
+            //TODO
+
+        }
+
+        public function __toString()
+        {
+            // TODO: Implement __toString() method.
+            return "";
+        }
+
+        private static function isComboWin(ArrayPieceQuantik $pieces): bool{
+
+            //TODO
+
+            return true;
+        }
+
+        private static function isPieceValide(ArrayPieceQuantik $pieces , PieceQuantik $p): bool{
+
+            //TODO
+
+            return true;
+        }
+
+
     }
 ?>
