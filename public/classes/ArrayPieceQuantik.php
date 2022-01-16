0fc59e80b891b210919f4c6fb3aa920c01a79d30
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-01-16 23:04:19 +0100

    Test Pieces

--- public/classes/ArrayPieceQuantik.php
+++ public/classes/ArrayPieceQuantik.php
@@ -3,7 +3,7 @@
     /**
      * Class nous permettant d'avoir un tableau avec des pièces dedans
      */
-    class ArrayPieceQuantik{
+    class ArrayPieceQuantik {
         protected array $piecesQuantiks;
         protected int $taille;
 
@@ -36,8 +36,13 @@
          * Obtenir une pièce spécifique du tableau
          * @param pos : Position du tableau que nous souhaitons récupérer
          */
-        public function getPieceQuantik(int $pos) : PieceQuantik {
-            return $this->piecesQuantiks[$pos];
+        public function getPieceQuantik(int $pos) {
+            if($pos < $this->getTaille()) {
+                return $this->piecesQuantiks[$pos];   
+            } else {
+                echo "Impossible d'afficher, hors tableau";
+                return;
+            }
         }
 
         /**
@@ -46,7 +51,10 @@
          * @param piece : Nouvelle pièce que nous souhaitons placer dans la position
          */
         public function setPieceQuantik(int $pos, PieceQuantik $piece) : void {
-            $this->piecesQuantiks[$pos] = $piece;
+            if($pos < $this->getTaille())
+                $this->piecesQuantiks[$pos] = $piece; 
+            else echo 'Insertion impossible';
+
             return;
         }
 
@@ -56,6 +64,7 @@
          */
         public function addPieceQuantik(PieceQuantik $piece) : void {
             $this->piecesQuantiks[] = $piece;
+            $this->taille++;
             return;
         }
 
@@ -65,6 +74,7 @@
          */
         public function removePieceQuantik(int $pos) : void {
             unset($this->piecesQuantiks[$pos]);
+            $this->taille--;
         }
 
         /**
@@ -127,4 +137,97 @@
             return $array;
         }
     }
-?>
+
+    /* TEST  */
+    
+    /* echo "==== Initialisation dun array de 4 pieces vides ====  <br>";
+    $array_void = new ArrayPieceQuantik();
+    echo $array_void;
+    */
+
+    // Résultat attendu 
+    /*
+    0.- Forme : 0 Couleur : WHITE
+    1.- Forme : 0 Couleur : WHITE
+    2.- Forme : 0 Couleur : WHITE
+    3.- Forme : 0 Couleur : WHITE
+    */
+    
+    /*echo '<br>';
+
+    echo "==== Initialisation dun array de 8 pieces noires ====  <br>";
+    $array_noires = ArrayPieceQuantik::initPiecesNoires();
+    echo $array_noires;
+    */
+
+    // Résultat attendu 
+    /*
+    0.- Forme : CONE Couleur : BLACK
+    1.- Forme : CONE Couleur : BLACK
+    2.- Forme : CUBE Couleur : BLACK
+    3.- Forme : CUBE Couleur : BLACK
+    4.- Forme : CYLINDRE Couleur : BLACK
+    5.- Forme : CYLINDRE Couleur : BLACK
+    6.- Forme : SPHERE Couleur : BLACK
+    7.- Forme : SPHERE Couleur : BLACK
+    */
+
+    /*echo '<br><br>';
+    
+    echo "==== Initialisation dun array de 8 pieces blanches ====  <br>";
+    $array_blanches = ArrayPieceQuantik::initPiecesBlanches();
+    echo $array_blanches;
+    */
+
+    /*
+    0.- Forme : CONE Couleur : WHITE
+    1.- Forme : CONE Couleur : WHITE
+    2.- Forme : CUBE Couleur : WHITE
+    3.- Forme : CUBE Couleur : WHITE
+    4.- Forme : CYLINDRE Couleur : WHITE
+    5.- Forme : CYLINDRE Couleur : WHITE
+    6.- Forme : SPHERE Couleur : WHITE
+    7.- Forme : SPHERE Couleur : WHITE
+    */
+
+    /*echo '<br><br>';
+
+    echo "==== Get Piece Hors Tableau ====  <br>";
+
+    echo $array_noires->getPieceQuantik(10);
+    */
+
+    // Résultat attendu
+    // Impossible d'afficher, hors tableau
+    /*echo '<br><br>';
+    echo "==== Get Piece ====  <br>";
+    echo $array_noires->getPieceQuantik(0);
+    */
+    // Résultat attendu
+    // Forme : CONE Couleur : BLACK
+    /*
+    echo '<br><br>';
+    echo "==== Add Piece ====  <br>";
+    $piece = PieceQuantik::initBlackCylindre();
+    $array_noires->addPieceQuantik($piece);
+    echo $array_noires->getPieceQuantik(8);
+    */
+    // Résultat attendu
+    // Forme : CYLINDRE Couleur : BLACK
+    /*
+    echo '<br><br>';
+    echo "==== Set Piece ====  <br>";
+    $array_noires->setPieceQuantik(8, PieceQuantik::initBlackSphere());
+    echo $array_noires->getPieceQuantik(8);
+    */
+    // Résultat attendu
+    // Forme : SPHERE Couleur : BLACK
+    /*
+    echo '<br><br>';
+    echo "==== Remove Piece ====  <br>";
+    $array_noires->removePieceQuantik(8);
+    echo $array_noires->getPieceQuantik(8);
+    */
+    // Résultat attendu
+    // Impossible d'afficher, hors tableau
+?>  
