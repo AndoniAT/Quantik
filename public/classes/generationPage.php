e452a96b548ef7d32077e686265d70abf9638738
bm190247 <mathieu.barton@etu.univ-lehavre.fr>
2022-01-17 13:38:09 +0100

    Méthode getDebutHTML et getFinHTML

--- public/classes/generationPage.php
+++ public/classes/generationPage.php
@@ -1,1 +1,23 @@
 <?php
+
+    class GenerationPage{
+
+        public function getDebutHTML():string{
+            return "<!DOCTYPE html>
+                    <html lang=\"en\">
+                    <head>
+                        <meta charset=\"UTF-8\">
+                        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+                        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+                        <title>Quantik</title>
+                    </head>
+                    <body>";
+        }
+
+        public function getFinHTML():string{
+            return "</body>
+                    </html>";
+        }
+
+
+    }
